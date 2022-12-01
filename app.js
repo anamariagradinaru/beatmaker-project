b11cfb48b33e9dec7c0bc05ef68e9844c8ee0b43
Ana-Maria Grădinaru <anamariagradinaru92@yahoo.com>
2022-12-01 20:37:22 +0200

    Made to hear the firsts sounds

--- app.js
+++ app.js
@@ -14,7 +14,26 @@ class DrumKit {
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        console.log(step);
+        // Loop over the pads
+        activeBars.forEach((bar) => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            //Check if pads are active
+            if (bar.classList.contains('active')) {
+                //Check each sound
+                if (bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
     start() {
@@ -29,6 +48,9 @@ class DrumKit {
 const drumKit = new DrumKit();
 drumKit.pads.forEach((pad) => {
     pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function () {
+        this.style.animation = '';
+    });
 });
 
 drumKit.playBtn.addEventListener('click', function () {
